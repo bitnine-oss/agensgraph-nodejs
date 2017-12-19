d1707f24a2556cdf45ac97f561e0f9af630c9c99
Beomyong Lee <bylee@bitnine.net>
2017-12-19 19:13:55 +0900

    test: Add test cases for graph data types
    Graph data types are GraphId, Vertex, Edge, Path, Vertex_Array and Edge_Array.

---
+++ test/config.js
@@ -0,0 +1,10 @@
+var Config = {
+    user: 'bylee',
+    password: 'agraph',
+    database: 'agens',
+    host: '127.0.0.1',
+    port: 5432
+};
+
+
+module.exports = Config
---
+++ test/graph/pathTest.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+var ag = require('../../lib');
+var config = require('../config');
+
+describe('PathTest suite', function() {
+    var client;
+    before('setUp', function(){
+        client = new ag.Client(config);
+        client.connect();
+        client.query('DROP GRAPH IF EXISTS gpt CASCADE');
+        client.query('CREATE GRAPH gpt');
+        client.query('SET graph_path = gpt');
+    });
+    after('tearDown', function(){
+        client.query('DROP GRAPH gpt CASCADE');
+        client.end();
+
+    });
+    it('testPath case 1', function(done) {
+        client.query("CREATE p=({s: '[}\\\"'})-[:e]->() RETURN p", [], function (err, res) {
+            if (err) throw err;
+
+            var p = res.rows[0].p;
+            assert.strictEqual(p.len(), 1);
+            
+            done();
+        });
+    });
+    it('testPath case 2', function(done){
+        client.query('MATCH ()-[r]->() RETURN count(*)', [], function (err, res) {
+            if (err) throw err;
+
+            assert.strictEqual(res.rows[0].count, '1');
+
+            done();
+        });
+    });
+});
