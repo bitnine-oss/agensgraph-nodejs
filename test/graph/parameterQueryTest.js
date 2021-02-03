b43b2c35b676ea174e0b7a13477aa3a683ec1782
ms.kwak <ms.kwak@bitnine.net>
2021-02-03 16:10:55 +0900

    fix test on ParameterQueryTest suite

--- test/graph/parameterQueryTest.js
+++ test/graph/parameterQueryTest.js
@@ -14,46 +14,46 @@ describe('ParameterQueryTest suite', function () {
     after('tearDown', function () {
         client.query('DROP GRAPH gpt CASCADE')
             .then(() => client.end());
-
     });
-    it('Test Cypher Types', function (done) {
+    it('Test Cypher Types', async function () {
         const q = {
             text: "CREATE (n:v {s: $1, l: $2, d: $3, f: $4, t: $5, a: $6, o: $7}) RETURN n",
-            types: [25, 23, 701, 16, 16, 1007, 3802],
             values: ['', 0, 0.0, false, true, [1, 2, 3], {name: 'bylee'}]
         }
-        client.query(q, function (err, res) {
-            if (err) throw err;
-
-            var v = res.rows[0].n.props;
-            assert.strictEqual(v.s, '');
-            assert.strictEqual(v.l, 0);
-            assert.strictEqual(v.d, 0.0);
-            assert.strictEqual(v.f, false);
-            assert.strictEqual(v.t, true);
-            assert.deepStrictEqual(v.a, [1, 2, 3]);
-            assert.deepStrictEqual(v.o, {name: 'bylee'});
-            done();
+        const wrappedValues = [];
+        q.values.forEach((value) => {
+            wrappedValues.push(JSON.stringify(value))
         });
+        q.values = wrappedValues;
+        const res = await client.query(q);
+        const v = res.rows[0].n.props;
+        assert.strictEqual(v.s, '');
+        assert.strictEqual(v.l, 0);
+        assert.strictEqual(v.d, 0.0);
+        assert.strictEqual(v.f, false);
+        assert.strictEqual(v.t, true);
+        assert.deepStrictEqual(v.a, [1, 2, 3]);
+        assert.deepStrictEqual(v.o, {name: 'bylee'});
     });
-    it('Test Objects and Arrays', function (done) {
+    it('Test Objects and Arrays', async function () {
         const q = {
             text: "CREATE (n:v {ss: $1, ll: $2, dd: $3, ff: $4, tt: $5, aa: $6, oo: $7}) RETURN n",
-            types: [1009, 1007, 1022, 1000, 1000, 1007, 3807],
             values: [['a', 'b', 'c'], [0, 1, 2], [0.0, 0.1, 0.2], [false, 0, '0'], [true, 1, '1'], [[1], [2], [3]], [{name: 'lee'}, {name: 'kim'}]]
         }
-        client.query(q, function (err, res) {
-            if (err) throw err;
-
-            var v = res.rows[0].n.props;
-            assert.deepStrictEqual(v.ss, ['a', 'b', 'c']);
-            assert.deepStrictEqual(v.ll, [0, 1, 2]);
-            assert.deepStrictEqual(v.dd, [0.0, 0.1, 0.2]);
-            assert.deepStrictEqual(v.ff, [false, false, false]);
-            assert.deepStrictEqual(v.tt, [true, true, true]);
-            assert.deepStrictEqual(v.aa, [[1], [2], [3]]);
-            assert.deepStrictEqual(v.oo, [{name: 'lee'}, {name: 'kim'}]);
-            done();
+        const wrappedValues = [];
+        q.values.forEach((value) => {
+            wrappedValues.push(JSON.stringify(value))
         });
+        q.values = wrappedValues;
+        const res = await client.query(q);
+
+        const v = res.rows[0].n.props;
+        assert.deepStrictEqual(v.ss, ['a', 'b', 'c']);
+        assert.deepStrictEqual(v.ll, [0, 1, 2]);
+        assert.deepStrictEqual(v.dd, [0.0, 0.1, 0.2]);
+        assert.deepStrictEqual(v.ff, [false, 0, '0']);
+        assert.deepStrictEqual(v.tt, [true, 1, '1']);
+        assert.deepStrictEqual(v.aa, [[1], [2], [3]]);
+        assert.deepStrictEqual(v.oo, [{name: 'lee'}, {name: 'kim'}]);
     });
 });
